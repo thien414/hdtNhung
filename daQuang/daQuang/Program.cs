c114ff43a79bcac69423a3a82dfd066d36fccd43
thien414 <lethanhthien414@gmail.com>
2020-07-29 20:38:34 +0700

    ok

--- daQuang/daQuang/Program.cs
+++ daQuang/daQuang/Program.cs
@@ -11,13 +11,16 @@ namespace daQuang
         static void Main(string[] args)
         {
             List<BankAccount> listAccount = new List<BankAccount>();
+            file_IO file = new file_IO();
+            file.DocFile(listAccount);
             int i = 0;
             do
             {
-                bool checkDN = true;
                 Console.WriteLine("-----------------Menu-----------------");
                 Console.WriteLine("-- 1. admin                         --");
                 Console.WriteLine("-- 2. nguoi dung                    --");
+                Console.WriteLine("-- 3. luu file                      --");
+                Console.WriteLine("-- 4. Thoat                         --");
                 Console.WriteLine("--------------------------------------\n");
                 Thread.Sleep(1000);
                 Console.Write("chon chuc nang :");
@@ -41,10 +44,13 @@ namespace daQuang
                                 Console.WriteLine("-- 2. Xoa                           --");
                                 Console.WriteLine("-- 3. Sua                           --");
                                 Console.WriteLine("-- 4. Tiem kiem                     --");
+                                Console.WriteLine("-- 5. Thoat                         --");
                                 Console.WriteLine("--------------------------------------\n");
-                                Thread.Sleep(3000);
+                                Thread.Sleep(1000);
                                 Console.Write("chon chuc nang :");
                                 checkCNadmin = int.Parse(Console.ReadLine());
+                                Console.WriteLine("dang xu ly !!!!");
+                                Thread.Sleep(3000);
                                 switch (checkCNadmin)
                                 {
                                     case 1:
@@ -55,7 +61,7 @@ namespace daQuang
                                         string idXoa = Console.ReadLine();
                                         Console.WriteLine("dang xu ly !!!!");
                                         Thread.Sleep(3000);
-                                        Xoa(listAccount,idXoa);
+                                        Xoa(listAccount, idXoa);
                                         Thread.Sleep(3000);
                                         break;
                                     case 3:
@@ -74,15 +80,16 @@ namespace daQuang
                                         TimKiem(listAccount, idTK);
                                         Thread.Sleep(3000);
                                         break;
-                                    default:
-                                        Console.WriteLine("dang xu ly !!!!");
-                                        Thread.Sleep(3000);
+                                    case 5:
                                         Console.WriteLine("Tam biet Admin !!!!");
-                                        Thread.Sleep(3000);
+                                        Thread.Sleep(2000);
+                                        break;
+                                    default:
+                                        Console.WriteLine("Nhap sai chuc nang !!!!");
+                                        Thread.Sleep(2000);
                                         break;
                                 }
-
-                            } while (checkCNadmin > 0 && checkCNadmin < 5);
+                            } while (checkCNadmin != 5);
                         }
                         else
                         {
@@ -93,13 +100,128 @@ namespace daQuang
                         }
                         break;
                     case 2:
+                        string id = DangNhap(listAccount);
+                        if (!id.Equals(""))
+                        {
+                            Console.WriteLine("dang xu ly !!!!");
+                            int checkCN;
+                            Thread.Sleep(3000);
+                            Console.WriteLine("dang nhap thanh cong !!!!");
+                            Thread.Sleep(3000);
+                            do
+                            {
+                                Console.WriteLine("-----------------Menu-----------------");
+                                Console.WriteLine("-- 1. Rut tien                      --");
+                                Console.WriteLine("-- 2. Chuyen tien                   --");
+                                Console.WriteLine("-- 3. Xem so du                     --");
+                                Console.WriteLine("-- 4. Thoat                         --");
+                                Console.WriteLine("--------------------------------------\n");
+                                Thread.Sleep(1000);
+                                Console.Write("chon chuc nang :");
+                                checkCN = int.Parse(Console.ReadLine());
+                                Console.WriteLine("dang xu ly !!!!");
+                                Thread.Sleep(3000);
+                                switch (checkCN)
+                                {
+                                    case 1:
+                                        int check = 0;
+                                        Console.Write("Nhap so tien can rut: ");
+                                        double mnRut = double.Parse(Console.ReadLine());
+                                        Console.WriteLine("dang xu ly !!!!");
+                                        Thread.Sleep(3000);
+                                        foreach (BankAccount AC in listAccount)
+                                        {
+                                            if (AC.ID.Equals(id))
+                                            {
+                                                if (AC.SoDu > mnRut)
+                                                {
+                                                    AC.SoDu -= mnRut;
+                                                    check = 1;
+                                                    Console.WriteLine("Rut tien thanh cong !!!");
+                                                    break;
+                                                }
+                                                else
+                                                {
+                                                    Console.WriteLine("So du khong du de thuc hien !!!");
+                                                    Thread.Sleep(3000);
+                                                }
+                                            }
+                                        }
+                                        if (check == 0) Console.WriteLine("Rut tien khong thanh cong !!!");
+                                        Thread.Sleep(3000);
+                                        break;
+                                    case 2:
+                                        int checkCh = 0;
+                                        Console.Write("Nhap so tien can chuyen: ");
+                                        double mnCh = double.Parse(Console.ReadLine());
+                                        Console.WriteLine("dang xu ly !!!!");
+                                        Thread.Sleep(3000);
+                                        foreach (BankAccount AC in listAccount)
+                                        {
+                                            if (AC.ID.Equals(id))
+                                            {
+                                                if (AC.SoDu > mnCh)
+                                                {
+                                                    AC.SoDu -= mnCh;
+                                                    checkCh = 1;
+                                                    Console.WriteLine("Chuyen tien thanh cong !!!");
+                                                    break;
+                                                }
+                                                else
+                                                {
+                                                    Console.WriteLine("So du khong du de thuc hien !!!");
+                                                    Thread.Sleep(3000);
+                                                }
+                                            }
+                                        }
+                                        if (checkCh == 0) Console.WriteLine("Chuyen tien khong thanh cong !!!");
+                                        Thread.Sleep(3000);
+                                        break;
+                                    case 3:
+                                        Console.WriteLine("dang xu ly !!!!");
+                                        Thread.Sleep(3000);
+                                        foreach (BankAccount AC in listAccount)
+                                        {
+                                            if (AC.ID.Equals(id))
+                                            {
+                                                Console.WriteLine("So du cua ban la : {0}", AC.SoDu);
+                                                break;
+                                            }
+                                        }
+                                        Thread.Sleep(3000);
+                                        break;
+                                    case 4:
+                                        Console.WriteLine("Tam biet !!!!");
+                                        Thread.Sleep(2000);
+                                        break;
+                                    default:
+                                        Console.WriteLine("Nhap sai chuc nang !!!!");
+                                        Thread.Sleep(2000);
+                                        break;
+                                }
+                            } while (checkCN != 4);
+                        }
+                        else
+                        {
+                            Console.WriteLine("dang xu ly !!!!");
+                            Thread.Sleep(3000);
+                            Console.WriteLine("dang nhap khong thanh cong !!!!\n");
+                            Thread.Sleep(3000);
+                        }
                         break;
-                    default:
+                    case 3:
+                        file.GhiFile(listAccount);
+                        break;
+                    case 4:
                         Console.WriteLine("Tam biet !!!!");
                         Thread.Sleep(2000);
                         break;
+                    default:
+                        Console.WriteLine("Nhap sai chuc nang !!!!");
+                        Thread.Sleep(2000);
+                        break;
                 }
-            } while (i > 0 && i < 3);
+            } while (i != 4);
            
             
         }
@@ -129,7 +251,7 @@ namespace daQuang
             
         }
 
-        static bool DangNhap(List<BankAccount> list)
+        static string DangNhap(List<BankAccount> list)
         {
             int check = 0;
             string id, pass;
@@ -147,14 +269,14 @@ namespace daQuang
                 {
                     if (id.Equals(AC.ID) && pass.Equals(AC.Password))
                     {
-                        return true;
+                        return AC.ID;
                     }
                 }
                 Console.WriteLine("ID hoac password sai vui long nhap lai!!!");
                 check++;
             } while (check > 0 && check < 4);
 
-            return false;
+            return "";
 
         }
 
@@ -177,13 +299,13 @@ namespace daQuang
             if (y == 0)
             {
                 AC.ID = checkID;
-                Console.Write("\nNhap password :");
+                Console.Write("Nhap password :");
                 AC.Password = Console.ReadLine().ToString();
-                Console.Write("\nNhap Ten chu the :");
+                Console.Write("Nhap Ten chu the :");
                 AC.TenTK = Console.ReadLine().ToString();
-                Console.Write("\nNhap loai tien :");
+                Console.Write("Nhap loai tien :");
                 AC.LoaiTien = Console.ReadLine().ToString();
-                Console.Write("\nNhap so du :");
+                Console.Write("Nhap so du :");
                 AC.SoDu = double.Parse(Console.ReadLine().ToString());
 
                 list.Add(AC);
@@ -208,14 +330,10 @@ namespace daQuang
 
         static void Sua(List<BankAccount> list, string id)
         {
-            int i = 0 ;
-            foreach (BankAccount check in list)
+            foreach (BankAccount AC in list)
             {
-                if (check.ID.Equals(id))
+                if (AC.ID.Equals(id))
                 {
-                    list.Remove(check);
-                    BankAccount AC = new BankAccount();
-
                     AC.ID = id;
                     Console.Write("\nNhap password :");
                     AC.Password = Console.ReadLine().ToString();
@@ -225,13 +343,11 @@ namespace daQuang
                     AC.LoaiTien = Console.ReadLine().ToString();
                     Console.Write("\nNhap so du :");
                     AC.SoDu = double.Parse(Console.ReadLine().ToString());
-
-                    list.Insert(i,AC);
+                    
                     Console.WriteLine("ID {0} da duoc sua thanh cong!!!", id);
 
                     return;
                 }
-                i++;
             }
             Console.WriteLine("ID {0} Khong ton tai!!!", id);
             
@@ -245,12 +361,13 @@ namespace daQuang
                 {
                     Console.WriteLine("ID {0} da duoc tim thay!!!", id);
                     Thread.Sleep(3000);
-                    check.toString();
+                    Console.WriteLine(check.toString());
                     return;
                 }
             }
             Console.WriteLine("ID {0} Khong ton tai!!!", id);
 
         }
+        
     }
 }
---
+++ daQuang/daQuang/file_IO.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace daQuang
+{
+    class file_IO
+    {
+        public void DocFile(List<BankAccount> list)
+        {
+            if (File.Exists(@"D:\\BankAccount.txt"))
+            {
+                string[] lines = File.ReadAllLines(@"D:\\BankAccount.txt");
+                for (int i = 0; i < lines.Length; i += 5)
+                {
+                    BankAccount AC = new BankAccount();
+
+                    AC.ID = lines[i];
+                    AC.Password = lines[i + 1];
+                    AC.TenTK = lines[i + 2];
+                    AC.LoaiTien = lines[i + 3];
+                    AC.SoDu = int.Parse(lines[i + 4]);
+
+                    list.Add(AC);
+                }
+            }
+        }
+        public void GhiFile(List<BankAccount> list)
+        {
+            if (File.Exists("D:\\BankAccount.txt"))
+            {
+                FileStream fs = new FileStream("D:\\BankAccount.txt", FileMode.Truncate, FileAccess.Write, FileShare.Write);
+                StreamWriter sWriter = new StreamWriter(fs, Encoding.UTF8);
+                foreach (BankAccount AC in list)
+                {
+                    sWriter.WriteLine(AC.ID);
+                    sWriter.WriteLine(AC.Password);
+                    sWriter.WriteLine(AC.TenTK);
+                    sWriter.WriteLine(AC.LoaiTien);
+                    sWriter.WriteLine(AC.SoDu);
+                }
+                sWriter.Flush();
+                fs.Close();
+            }
+            else
+            {
+                FileStream fs = new FileStream("D:\\BankAccount.txt", FileMode.Create, FileAccess.Write, FileShare.Write);
+                StreamWriter sWriter = new StreamWriter(fs, Encoding.UTF8);
+                foreach (BankAccount AC in list)
+                {
+                    sWriter.WriteLine(AC.ID);
+                    sWriter.WriteLine(AC.Password);
+                    sWriter.WriteLine(AC.TenTK);
+                    sWriter.WriteLine(AC.LoaiTien);
+                    sWriter.WriteLine(AC.SoDu);
+                }
+                sWriter.Flush();
+                fs.Close();
+            }
+        }
+            
+    }
+}
